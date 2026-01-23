bf0e746658f8d346d704e63267276fe40ec9a4cb
VaradK62442 <varadkulk123@gmail.com>
2026-01-23 13:35:26 +0000

    add remaining methods

--- src/algmatch/stableMatchings/studentProjectAllocation/ties/instanceGenerators/__init__.py
+++ src/algmatch/stableMatchings/studentProjectAllocation/ties/instanceGenerators/__init__.py
@@ -1,3 +1,8 @@
 from .abstract import AbstractInstanceGenerator as SPASTIG_Abstract
 from .random import SPASTIG_Random
 from .euclidean import SPASTIG_Euclidean
+from .reverse import SPASTIG_ReverseEuclidean
+from .expectations import SPASTIG_ExpectationsEuclidean
+from .fame import SPASTIG_FameEuclidean
+from .attributes import SPASTIG_Attributes
+from .fameExtended import SPASTIG_FameEuclideanExtended
---
+++ src/algmatch/stableMatchings/studentProjectAllocation/ties/instanceGenerators/expectations.py
@@ -0,0 +1,28 @@
+"""
+Program to generate an instance of SPA-ST - Expectations Euclidean
+Student Project Allocation with Student preferences over projects allowing ties
+"""
+
+import numpy as np
+
+from algmatch.stableMatchings.studentProjectAllocation.SPA_P.instanceGenerators.euclidean import SPAPIG_Euclidean
+
+
+class SPASTIG_ExpectationsEuclidean(SPAPIG_Euclidean):
+    def __init__(
+            self,
+            num_dimensions: int = 5,
+            stdev: float = 0.5,
+            **kwargs,
+    ):
+        super().__init__(num_dimensions=num_dimensions, **kwargs)
+        self._stdev = stdev
+
+
+    def sample_all_points(self):
+        super().sample_all_points()
+
+        self._project_points = np.random.normal(
+            loc=self._project_points,
+            scale=self._stdev,
+        )
