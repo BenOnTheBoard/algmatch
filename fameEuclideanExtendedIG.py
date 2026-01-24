8cd2c60ebc59b24bc551c16113ce606c468507e0
VaradK62442 <varadkulk123@gmail.com>
2026-01-24 15:58:09 +0000

    separate files for experiments per IG

---
+++ fameEuclideanExtendedIG.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from algmatch.stableMatchings.studentProjectAllocation.ties.spastExperiments import run_experiment
+from algmatch.stableMatchings.studentProjectAllocation.ties.instanceGenerators import SPASTIG_FameEuclideanExtended
+
+for n1 in range(100, 1001, 100):
+    for sd in np.arange(0, 0.1, 0.1):
+        for ld in np.arange(0, 0.1, 0.1):
+            sd, ld = round(sd, 2), round(ld, 2)
+            run_experiment(
+                SPASTIG_FameEuclideanExtended,
+                n1, 1, n1 // 2,
+                n1 // 2, n1 // 5,
+                "fameExtended",
+                f"fameExtended_{n1}_{sd}_{ld}",
+                root_folder="results",
+                runs=100,
+                student_tie_density=sd,
+                lecturer_tie_density=ld,
+            )
