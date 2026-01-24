56393ceec59bb85ce120a3081441e7b9e566f401
VaradK62442 <varadkulk123@gmail.com>
2026-01-24 15:55:06 +0000

    add more options to running experiments

--- src/algmatch/stableMatchings/studentProjectAllocation/ties/spastExperiments.py
+++ src/algmatch/stableMatchings/studentProjectAllocation/ties/spastExperiments.py
@@ -2,17 +2,12 @@ from pathlib import Path
 from tqdm import tqdm
 import time
 import numpy as np
+import os
 
 from spastSolver import GurobiSPAST
 from algmatch.stableMatchings.studentProjectAllocation.ties.instanceGenerators import (
     SPASTIG_Abstract,
-    SPASTIG_Random,
     SPASTIG_Euclidean,
-    SPASTIG_ReverseEuclidean,
-    SPASTIG_ExpectationsEuclidean,
-    SPASTIG_FameEuclidean,
-    SPASTIG_Attributes,
-    SPASTIG_FameEuclideanExtended
 )
 
 def run_experiment(
@@ -22,25 +17,31 @@ def run_experiment(
     upper_bound: int,
     num_projects: int,
     num_lecturers: int,
-    foldername: str,
+    experiment_folder: str,
     filename_prefix: str,
+    root_folder: str = "experiments",
     runs: int = 100,
+    save_data: bool = True,
+    save_results: bool = True,
     **kwargs
 ):
     s = generation_method(
-        num_students,
-        lower_bound,
-        upper_bound,
-        num_projects,
-        num_lecturers,
+        num_students = num_students,
+        lower_bound = lower_bound,
+        upper_bound = upper_bound,
+        num_projects = num_projects,
+        num_lecturers = num_lecturers,
         **kwargs
     )
-    EXPERIMENTS_FOLDER = "experiments"
-    Path(f"{EXPERIMENTS_FOLDER}/{foldername}").mkdir(parents=True, exist_ok=True)
+    base_folder = f"{root_folder}/{experiment_folder}"
+    data_folder = f"{base_folder}_data"
+    results_folder = f"{base_folder}_results"
+    Path(data_folder).mkdir(parents=True, exist_ok=True)
+    Path(results_folder).mkdir(parents=True, exist_ok=True)
     time_results = np.zeros(runs)
 
     for i in tqdm(range(runs)):
-        filename = f"{EXPERIMENTS_FOLDER}/{foldername}/{filename_prefix}_{i}.txt"
+        filename = f"{data_folder}/{filename_prefix}{'_'+str(i) if save_data else ''}.txt"
         s.generate_instance()
         s.write_instance_to_file(filename)
 
@@ -50,9 +51,15 @@ def run_experiment(
         end_time = time.time()
         time_results[i] = end_time - start_time
 
-    with open(f"{EXPERIMENTS_FOLDER}/results_{foldername}.txt", "w") as f:
-        print(f"""
-Folder name: {foldername}
+    if not save_data:
+        for file in os.listdir(data_folder):
+            os.remove(os.path.join(data_folder, file))
+        Path(data_folder).rmdir()
+
+    if save_results:
+        with open(f"{root_folder}/{experiment_folder}_results/results_{experiment_folder}_{filename_prefix}.txt", "w") as f:
+            print(f"""
+Folder name: {experiment_folder}
 Average time taken (seconds): {time_results.mean()}
 Params:
     generation_method: {generation_method}
@@ -63,16 +70,16 @@ Params:
     num_lecturers: {num_lecturers}
     runs: {runs}
     kwargs: {kwargs}
-    """, file=f)
+        """, file=f)
 
 
 if __name__ == "__main__":
     n1 = 100
     run_experiment(
-        SPASTIG_Random,
+        SPASTIG_Euclidean,
         n1, 1, n1 // 2,
         n1 // 2, n1 // 5,
         "random",
         "random100",
-        100
+        runs = 100
     )
