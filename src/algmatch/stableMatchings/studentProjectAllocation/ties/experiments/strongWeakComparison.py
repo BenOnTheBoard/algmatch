c5c9dce373863765ac7380c6d218aa71c5643da8
Varad Kulkarni <varadkulk123@gmail.com>
2026-02-12 14:17:12 +0000

    add cluster dir to store data and tqdm formatting

--- src/algmatch/stableMatchings/studentProjectAllocation/ties/experiments/strongWeakComparison.py
+++ src/algmatch/stableMatchings/studentProjectAllocation/ties/experiments/strongWeakComparison.py
@@ -5,6 +5,7 @@ this module compares the matching sizes of strong and weak solvers for SPAST ins
 import time
 import numpy as np
 from pathlib import Path
+from numpy._core.numeric import False_
 from tqdm import tqdm
 
 from algmatch.stableMatchings.studentProjectAllocation.ties.spastStrongSolver import SPASTStrongSolver
@@ -56,13 +57,14 @@ def compare_matching_sizes(
 def main():
     NUM_STUDENTS = 25
     ITERS = 100
-    Path("data").mkdir(parents=True, exist_ok=True)
-    Path("results").mkdir(parents=True, exist_ok=True)
+    CLUSTER_DIR="."
+    Path(CLUSTER_DIR + "data").mkdir(parents=True, exist_ok=True)
+    Path(CLUSTER_DIR + "results").mkdir(parents=True, exist_ok=True)
 
-    for sd in np.arange(0, 1, 0.1):
-        for ld in np.arange(0, 1, 0.1):
+    for sd in tqdm(np.arange(0, 1, 0.1), position=0, leave=False, desc="sd"):
+        for ld in tqdm(np.arange(0, 1, 0.1), position=1, leave=False, desc="ld"):
             times: list[tuple[float, int, float, int]] = []
-            for i in tqdm(range(ITERS)):
+            for _ in tqdm(range(ITERS), position=2, leave=False, desc="it"):
                 sd, ld = round(sd, 2), round(ld, 2)
                 times.append(
                     compare_matching_sizes(
@@ -70,11 +72,11 @@ def main():
                         sd,
                         ld,
                         NUM_STUDENTS // 2,
-                        f"data/instance_{int(sd*100)}_{int(ld*100)}_{i}.txt"
+                        CLUSTER_DIR + "data/instance_{int(sd*100)}_{int(ld*100)}_{i}.txt"
                     )
                 )
 
-            with open(f"results/instance_{int(sd*100)}_{int(ld*100)}.csv", "w") as f:
+            with open(CLUSTER_DIR + "results/instance_{int(sd*100)}_{int(ld*100)}.csv", "w") as f:
                 f.write("Weak Time (ns),Weak Size,Strong Time (ns),Strong Size,Time Difference (ns)\n")
                 f.writelines(f"{weak_time},{weak_size},{strong_time},{strong_size},{weak_time - strong_time}\n" for weak_time, weak_size, strong_time, strong_size in times)
 
