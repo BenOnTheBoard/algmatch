c3a6207801da6df37c19597af7deb20b31403356
Varad Kulkarni <varadkulk123@gmail.com>
2026-02-26 15:06:47 +0000

    record times and other optimisations

--- strongProbabilityTiming.py
+++ strongProbabilityTiming.py
@@ -2,6 +2,8 @@ from tqdm import tqdm
 from pathlib import Path
 import numpy as np
 from random import choice
+import inspect
+from time import perf_counter_ns
 
 from concurrent.futures import ProcessPoolExecutor
 from itertools import product
@@ -34,8 +36,8 @@ GENERATORS = [
 ITERS = 100
 
 
-def combination_generation(*args):
-    return choice(GENERATORS)(*args)
+def combination_generation(*args, **kwargs):
+    return choice(GENERATORS)(*args, **kwargs)
 
 
 def run_experiment(
@@ -46,36 +48,51 @@ def run_experiment(
     iters,
     generation_method
 ):
+    def _init_gen_method():
+        return generation_method(
+            num_students=num_students,
+            lower_bound=pref_list_length,
+            upper_bound=pref_list_length,
+            num_projects=num_students // 2,
+            num_lecturers=num_students // 5,
+            student_tie_density=student_tie_density,
+            lecturer_tie_density=lecturer_tie_density,
+        )
+
     results = {
         "soln_exi": 0,
-        "soln_dne": 0
+        "soln_dne": 0,
+        "times": []
     }
-    generator = generation_method(
-        num_students=num_students,
-        lower_bound=pref_list_length,
-        upper_bound=pref_list_length,
-        num_projects=num_students // 2,
-        num_lecturers=num_students // 5,
-        student_tie_density=student_tie_density,
-        lecturer_tie_density=lecturer_tie_density,
-    )
+    generator = _init_gen_method()
+    using_combination = inspect.isfunction(generation_method)
+    generator_name = "SPASTIG_Combination" if using_combination else str(generator)
 
     foldername = '_'.join([
         str(num_students), str(pref_list_length),
         str(int(student_tie_density*100)),
         str(int(lecturer_tie_density*100)),
-        str(generator) if isinstance(generator, SPASTIG_Abstract) else "SPASTIG_Combination"
+        generator_name
     ])
     Path(CLUSTER_DIR + f"data/{foldername}").mkdir(parents=True, exist_ok=True)
 
     for i in range(iters):
         filename = CLUSTER_DIR + f"data/{foldername}/instance_{i}.txt"
+
+        if using_combination:
+            generator = _init_gen_method()
+
         generator.generate_instance()
         generator.write_instance_to_file(filename)
+        with open(filename, "a") as f:
+            f.write(str(generator))
 
         spast_strong = SPASTStrongSolver(filename, output_flag=0)
         spast_strong.J.setParam("Threads", 1)
+        start = perf_counter_ns()
         spast_strong.solve()
+        end = perf_counter_ns()
+        results["times"].append(end - start)
 
         if spast_strong.assignments_as_dict():
             results["soln_exi"] += 1
@@ -83,19 +100,9 @@ def run_experiment(
             results["soln_dne"] += 1
 
     with open(CLUSTER_DIR + f"results/{foldername}_results.txt", "w") as f:
-        f.write(f"""Iters: {iters}
-Params:
-    Num students: {num_students}
-    Preference list length: {pref_list_length}
-    Student tie density: {student_tie_density}
-    Lecturer tie density: {lecturer_tie_density}
-
-Results:
-    Solution exists: {results["soln_exi"]}
-    Solution does not exist: {results["soln_dne"]}
-
-{results["soln_exi"]},{results["soln_dne"]}
-""")
+        f.write(f"{results["soln_exi"]},{results["soln_dne"]}\n")
+        for time in results["times"]:
+            f.write(f"{time}\n")
 
 def run_instance(n1: int, sd: float, ld: float, gen: SPASTIG_Abstract):
     sd, ld = round(sd, 2), round(ld, 2)
