b647db0f69410e0ae434c8cda471b4fcfa833249
Varad Kulkarni <varadkulk123@gmail.com>
2026-03-01 17:46:42 +0000

    fn for plotting avg times

--- strongProbabilityAnalysis.py
+++ strongProbabilityAnalysis.py
@@ -7,22 +7,16 @@ from algmatch.stableMatchings.studentProjectAllocation.ties.experiments.strongPr
     combination_generation
 )
 
-"""
-For each file in RESULTS_DIR, plot a heatmap grid with
-- tie density for lecturers as x-axis (second param)
-- tie density for students as y-axis (first param)
-- probability of stable matching existence
-
-File format for filename:
-    <n1>_<pref list length>_<student tie density>_<lecturer tie density>_<gen method>_results.txt
-First line: <solution exists count>,<solution doesnt exist count>
-Next ITERS lines:
-    time taken in ns for each run
-"""
-
 RESULTS_DIR = "./results/"
 ITERS = 25
 OUTPUT_DIR = "./"
+DUMMY_ARGS = {
+    "num_students": 100,
+    "lower_bound": 10,
+    "upper_bound": 10,
+    "num_projects": 50,
+    "num_lecturers": 20
+}
 
 SDs = np.arange(0, 0.41, 0.05)
 LDs = np.arange(0, 0.41, 0.05)
@@ -33,15 +27,8 @@ filename = lambda n1, sd, ld, gen_name: f"{RESULTS_DIR}{n1}_{n1 // 10}_{int(sd*1
 def plot_heatmap(n1):
     for generation_method in GENERATORS + [combination_generation]:
         # dummy initialisation
-        generator = generation_method(
-            num_students=100,
-            lower_bound=10,
-            upper_bound=10,
-            num_projects=50,
-            num_lecturers=20
-        )
+        generator = generation_method(**DUMMY_ARGS)
         generator_name = get_gen_name(generator, generation_method)
-        print(generator_name)
         heatmap = np.zeros((len(SDs), len(LDs)))
         for i, sd in enumerate(SDs):
             for j, ld in enumerate(LDs):
@@ -62,10 +49,44 @@ def plot_heatmap(n1):
         plt.xlabel("Lecturer Tie Density")
         plt.ylabel("Student Tie Density")
         plt.colorbar(label="Probability of Stable Matching Existence")
-        plt.title(f"{generator_name} - Probability Heatmap for {n1} students")
+        plt.title(f"Probability Heatmap ({n1} students)\n{generator_name}")
         plt.savefig(f"{OUTPUT_DIR}heatmap_{generator_name}.jpg")
         plt.close()
 
 
+def plot_avg_times(n1):
+    for generation_method in GENERATORS + [combination_generation]:
+        # dummy initialisation
+        generator = generation_method(**DUMMY_ARGS)
+        generator_name = get_gen_name(generator, generation_method)
+        avg_times = np.zeros((len(SDs), len(LDs)))
+        highest = -1
+
+        for i, sd in enumerate(SDs):
+            for j, ld in enumerate(LDs):
+                with open(filename(n1, sd, ld, generator_name), "r") as f:
+                    times = map(float, f.readlines()[1:])
+                    avg_times[i, j] = 1e-9 * sum(times) / ITERS
+                    highest = max(highest, avg_times[i, j])
+
+        X, Y = np.meshgrid(LDs, SDs)
+        plt.pcolormesh(
+            X, Y,
+            avg_times,
+            cmap="magma_r",
+            vmin=0,
+            vmax=highest,
+            shading="auto",
+            edgecolor="k",
+            linewidth=0.5
+        )
+        plt.xlabel("Lecturer Tie Density")
+        plt.ylabel("Student Tie Density")
+        plt.colorbar(label="Average Time (seconds)")
+        plt.title(f"Average Time ({n1} students)\n{generator_name}")
+        plt.savefig(f"{OUTPUT_DIR}avg_time_{generator_name}.jpg")
+        plt.close()
+
 if __name__ == "__main__":
     plot_heatmap(100)
+    plot_avg_times(100)
