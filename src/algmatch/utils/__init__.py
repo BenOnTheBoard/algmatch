533cbb6c78c6cd15641e782ba71d13a56f240929
Ben Whitehead <ben.pi.whitehead@gmail.com>
2026-03-09 20:12:25 +0000

    exports enumeration utilities from algmatch.utils

--- src/algmatch/utils/__init__.py
+++ src/algmatch/utils/__init__.py
@@ -1,4 +1,4 @@
-# === Instance Generators ===
+# === Instance Generation ===
 
 from algmatch.utils.generators.SM.smGenerator import SMGenerator
 from algmatch.utils.generators.SM.smtGenerator import SMTGenerator
@@ -11,3 +11,15 @@ from algmatch.utils.generators.SPAS.spastGenerator import SPASTGenerator
 
 from algmatch.utils.generators.SR.srGenerator import SRGenerator
 
+# === Enumeration ===
+
+from algmatch.utils.enumerators.SM.smEnumerator import SMEnumerator
+from algmatch.utils.enumerators.SM.smtEnumerator import SMTEnumerator
+
+from algmatch.utils.enumerators.HR.hrEnumerator import HREnumerator
+from algmatch.utils.enumerators.HR.hrtEnumerator import HRTEnumerator
+
+from algmatch.utils.enumerators.SPAS.spasEnumerator import SPASEnumerator
+from algmatch.utils.enumerators.SPAS.spastEnumerator import SPASTEnumerator
+
+from algmatch.utils.enumerators.SR.srEnumerator import SREnumerator
