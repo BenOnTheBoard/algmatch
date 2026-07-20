282a0d25b16072da273535560d6401d6a047f7ae
Ben Whitehead <ben.pi.whitehead@gmail.com>
2026-07-20 22:23:05 +0100

    AbstractPreferenceInstance now has unified loading procedure

--- src/algmatch/abstractClasses/abstractPreferenceInstance.py
+++ src/algmatch/abstractClasses/abstractPreferenceInstance.py
@@ -3,38 +3,21 @@ Abstract class to store preference lists for both sides in a type of matching pr
 """
 
 from itertools import product
-import os
 
+from algmatch.abstractClasses.preferenceSource import PreferenceSource
 from algmatch.errors.InstanceSetupErrors import PrefRepError, PrefNotFoundError
 
 
 class AbstractPreferenceInstance:
-    def __init__(
-        self, filename: str | None = None, dictionary: dict | None = None
-    ) -> None:
-        assert filename is not None or dictionary is not None, (
-            "Either filename or dictionary must be provided"
-        )
-        assert not (filename is not None and dictionary is not None), (
-            "Only one of filename or dictionary must be provided"
-        )
-
-        if filename is not None:
-            assert os.path.isfile(filename), f"File {filename} does not exist"
-            self._load_from_file(filename)
-
-        if dictionary is not None:
-            self._load_from_dictionary(dictionary)
+    def __init__(self, source: PreferenceSource) -> None:
+        self._load(source)
 
     def _general_setup_procedure(self):
         self.check_preference_lists()
         self.clean_unacceptable_pairs()
         self.set_up_rankings()
 
-    def _load_from_file(self, filename: str) -> None:
-        raise NotImplementedError("Method not implemented")
-
-    def _load_from_dictionary(self, dictionary: dict) -> None:
+    def _load(self, source: PreferenceSource) -> None:
         raise NotImplementedError("Method not implemented")
 
     def check_preference_lists(self) -> None:
